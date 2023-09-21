384b81109595238cf661e81970a6adfaef7d852c
Ahmed <ahmedbabar@cmail.carleton.ca>
2023-09-21 14:00:26 -0400

    Yess

---
+++ SYSC3110Lab1_101154651/Lab1 - Copy/AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class AddressBook {
+
+    private final HashMap<BuddyInfo, String> addressBook;
+
+    public AddressBook(HashMap<BuddyInfo, String> addressBook) {
+        this.addressBook = addressBook;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+    public void addBuddy(String num, String name, String address){
+        BuddyInfo newFriend = new BuddyInfo(num,name,address);
+        addressBook.put(newFriend, name);
+    }
+
+    public void removeBuddy(String name){
+        /* What if there are 2 of the same name? */
+    }
+
+
+
+
+
+
+
+
+
+}
